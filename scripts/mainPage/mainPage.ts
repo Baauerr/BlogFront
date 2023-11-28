583b13c19c3990ee9acd7ee8de3abbd0549e9f7c
Artyom Bauer <artem_bauer@inbox.ru>
2023-11-29 02:12:40 +0700

    Работа над главной страницей Добавлено отображение постов (текст можно скрывать и раскрывать (боль)) Отображаютсяя картиночки(ура) Ещё не закончено, но уже что-то

--- scripts/mainPage/mainPage.ts
+++ scripts/mainPage/mainPage.ts
@@ -6,20 +6,59 @@ export class mainPageData{
     min: number | null;
     max: number | null;
     sorting: string | null;
-    onlyMyCommunites: boolean | null;
+    onlyMyCommunities: boolean | null;
     page: number;
     size: number;
 }
 
-const data: mainPageData = {
+const formData: mainPageData = {
     tags: null,
     author: null,
     min: null,
     max: null,
     sorting: "LikeAsc",
-    onlyMyCommunites: false,
+    onlyMyCommunities: false,
     page: 1,
     size: 5,
 }
+function loadMainPage(){
+    
+}
 
-getInfoOnPage(data);
+getInfoOnPage(formData);
+
+
+function collectFormData() {
+    const formData = new mainPageData();
+
+    formData.tags = Array.from((document.querySelectorAll('#filterSquare option:checked') as NodeListOf<HTMLOptionElement>)).map(option => option.value);
+    formData.author = (document.getElementById('inputWide') as HTMLInputElement).value;
+    formData.min = parseFloat((document.getElementById('input2') as HTMLInputElement).value);
+    formData.max = parseFloat((document.getElementById('input3') as HTMLInputElement).value);
+    formData.sorting = (document.getElementById('filterSingle') as HTMLSelectElement).value;
+    formData.onlyMyCommunities = (document.getElementById('only_my_groups') as HTMLInputElement).checked;
+
+    const activePage = document.querySelector('#pagination .page-item.active a') as HTMLElement;
+    formData.page = activePage ? parseInt(activePage.textContent) : 1;
+    formData.size = 5;
+
+    return formData;
+}
+
+function applyFormDataToClass() {
+    const formData = collectFormData();
+    getInfoOnPage(formData);
+    const updateHash = () => {
+        const queryString = Object.entries(formData)
+          .filter(([key, value]) => value !== null && value !== "" && !(Array.isArray(value) && value.length === 0))
+          .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+          .join('&');
+      
+        location.hash = queryString;
+      };
+      
+      updateHash();
+    console.log(formData);
+}
+
+document.getElementById('apply_button').addEventListener('click', applyFormDataToClass);
--- scriptsJS/api/mainPageAPI.js
+++ scriptsJS/api/mainPageAPI.js
@@ -1,10 +1,10 @@
 export async function getInfoOnPage(filterData) {
     const queryString = Object.entries(filterData)
-        .filter(([key, value]) => value !== null)
+        .filter(([key, value]) => value !== null && value !== "" && !(Array.isArray(value) && value.length === 0))
         .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
         .join('&');
     console.log(queryString);
-    await fetch('https://blog.kreosoft.space/api/post?${queryString}', {
+    await fetch(`https://blog.kreosoft.space/api/post?${queryString}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
